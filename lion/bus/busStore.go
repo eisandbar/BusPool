652d53e38b36a9b876334c1294eeea9990b93566
eisandbar <eisandbar@gmail.com>
2022-08-30 23:17:56 -0400

    feat(lion): Add MemoryBusStore
    Added a MemoryBusStore that will have bus locations in memory
    Added tests that make sure FindBus works as intended

--- lion/bus/busStore.go
+++ lion/bus/busStore.go
@@ -1,9 +1,42 @@
 package bus
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/eisandbar/BusPool/lion/types"
 )
 
 type BusStore interface {
 	FindBus(point types.GeoPoint) int
+}
+
+type Bus struct {
+	Id       int
+	Location types.GeoPoint
+}
+
+type MemoryBusStore struct {
+	bus []Bus
+	sync.RWMutex
+}
+
+// Init bus slice mostly for testing
+func (bs *MemoryBusStore) Init(bus []Bus) {
+	bs.bus = bus
+}
+
+// Find bus goes over the list of buses and finds the nearest one
+func (bs *MemoryBusStore) FindBus(point types.GeoPoint) int {
+	bs.RLock()
+	defer bs.RUnlock()
+	id, dist := bs.bus[0].Id, point.Distance(bs.bus[0].Location.LatLng).Abs().Degrees()
+	for _, bus := range bs.bus {
+		fmt.Println(dist, point.Distance(bus.Location.LatLng).Abs().Degrees())
+		if point.Distance(bus.Location.LatLng).Abs().Degrees() < dist {
+			id = bus.Id
+			dist = point.Distance(bus.Location.LatLng).Abs().Degrees()
+		}
+	}
+	return id
 }
---
+++ lion/bus/busStore_test.go
@@ -0,0 +1,34 @@
+package bus_test
+
+import (
+	"testing"
+
+	"github.com/eisandbar/BusPool/lion/bus"
+	"github.com/eisandbar/BusPool/lion/types"
+	"github.com/golang/geo/s2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindBus(t *testing.T) {
+	bs := bus.MemoryBusStore{}
+	buses := []bus.Bus{
+		{Id: 1, Location: types.GeoPoint{LatLng: s2.LatLngFromDegrees(19, 13)}},
+		{Id: 2, Location: types.GeoPoint{LatLng: s2.LatLngFromDegrees(21, 15)}},
+		{Id: 3, Location: types.GeoPoint{LatLng: s2.LatLngFromDegrees(22, 22)}},
+	}
+	bs.Init(buses)
+
+	testData := []struct {
+		point types.GeoPoint
+		id    int
+	}{
+		{point: types.GeoPoint{LatLng: s2.LatLngFromDegrees(22, 16)}, id: 2},
+		{point: types.GeoPoint{LatLng: s2.LatLngFromDegrees(22, 13)}, id: 2},
+		{point: types.GeoPoint{LatLng: s2.LatLngFromDegrees(15, 13)}, id: 1},
+		{point: types.GeoPoint{LatLng: s2.LatLngFromDegrees(21, 21)}, id: 3},
+	}
+
+	for _, tt := range testData {
+		assert.Equal(t, tt.id, bs.FindBus(tt.point))
+	}
+}
