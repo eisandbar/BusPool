77e4a26d38c6a0f4a1121641eaf8032aa5261cea
eisandbar <eisandbar@gmail.com>
2022-08-31 21:02:31 -0400

    feat(lion): Add PathFinder
    Added interface PathFinder and type DumbPathFinder
    DumbPathFinder uses bus points and the new geopoint to create a request to the local GraphHopper API
    to get the path the bus should take to reach all listed points.
    Path is returned as a polyline encoded string.
    Added integration test to make sure it worked with local GraphHopper server

---
+++ lion/path/pathFinder.go
@@ -0,0 +1,75 @@
+package path
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/eisandbar/BusPool/lion/bus"
+	"github.com/eisandbar/BusPool/lion/types"
+)
+
+var GraphHopper = "http://localhost:8989"
+
+type PathFinder interface {
+	GetPath(bus.Bus, types.GeoPoint) (string, error)
+}
+
+// This path finder will add a point to the end of points needed to visit
+type DumbPathFinder struct {
+}
+
+// Returns a polyline encoded array of coordinates
+func (pf DumbPathFinder) GetPath(bus bus.Bus, point types.GeoPoint) (string, error) {
+	req := generateRequest(bus, point)
+	resp, err := http.Post(fmt.Sprintf("%s/route", GraphHopper), "application/json", req)
+	if err != nil {
+		return "", errors.New("Failed to get directions from GraphHopper")
+	}
+	var res response
+	body, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Printf("%+v\n %s", resp, body)
+	if err != nil {
+		return "", errors.New("Failed to read response body")
+	}
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return "", errors.New("Failed to unmarshal json")
+	}
+	if len(res.Paths) < 1 {
+		return "", errors.New("Failed to find a path")
+	}
+	return res.Paths[0].Points, nil
+}
+
+func generateRequest(bus bus.Bus, point types.GeoPoint) io.Reader {
+	points := make([][]float64, 0, len(bus.Points)+1)
+	for _, p := range bus.Points {
+		points = append(points, []float64{p.Lng.Degrees(), p.Lat.Degrees()})
+	}
+	points = append(points, []float64{point.Lng.Degrees(), point.Lat.Degrees()})
+	req := request{
+		Points:       points,
+		Instructions: false,
+		Optimize:     false,
+	}
+	body, _ := json.Marshal(req)
+	return bytes.NewBuffer(body)
+}
+
+type request struct {
+	Points       [][]float64 `json:"points"`
+	Instructions bool        `json:"instructions"`
+	Optimize     bool        `json:"optimize"`
+}
+
+type response struct {
+	Paths []struct {
+		Points string
+	}
+}
