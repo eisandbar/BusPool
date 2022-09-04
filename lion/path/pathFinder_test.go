bfb12e1fbe931e51d638c7f7b7f077c197ef1ba8
eisandbar <eisandbar@gmail.com>
2022-09-03 22:05:31 -0400

    test(lion): Add comment on expected behavior
    Added comment on the expected string in the test
    String should have only a singular \, but expectedString has two as the \ needs to be escaped

--- lion/path/pathFinder_test.go
+++ lion/path/pathFinder_test.go
@@ -18,6 +18,7 @@ func TestGetPath(t *testing.T) {
 	}
 	point := types.GeoPoint{LatLng: s2.LatLngFromDegrees(52.5, 13.4)}
 
+	// The \\ in the expected string is actually just a single \, but it had to be escaped
 	expectedString := "gvp_I{nrpAnI@TFBsd@vCgAf@CTNTh@X^JFVBTCXMLUX_ATc@XKdFXvGv@xCFpLzAX@lBG`AK\\K^YbJsLhPgU`UyZzFiGT_@BY?O~@?jDKdB]X_DTsFAyEKqDa@wJGg@m@gD}AUaAHw@TQsAEIQGqB}A_J_EFw@Ci@aCcNLKNApA?FBBFZtBdBBPDnA~Az@@DGBKDw@BG"
 	res, err := pf.GetPath(testBus, point)
 	assert.NoError(t, err)
