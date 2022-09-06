3f1a4c21a32e0f21abb1de63dd2a24bd3cc93f6c
eisandbar <eisandbar@gmail.com>
2022-09-06 02:31:20 -0400

    feat(client): Add load function
    Added a function that given a file path can load geojson data
    and convert it into a 2d slice of [lat, lon] float64

---
+++ client/load.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+)
+
+func loadData(file string) [][]float64 {
+	dataFile, err := os.Open(file)
+	defer dataFile.Close()
+	if err != nil {
+		log.Fatalf("Failed to open data file, %s", err)
+	}
+
+	byteData, err := io.ReadAll(dataFile)
+	if err != nil {
+		log.Fatalf("Failed to read data file, %s", err)
+	}
+
+	var geoData geoJson
+	json.Unmarshal(byteData, &geoData)
+
+	coordinates := make([][]float64, len(geoData.Features))
+	for i, feature := range geoData.Features {
+		// The json has it in [lon, lat]
+		coordinates[i] = []float64{feature.Geometry.Coordinates[1], feature.Geometry.Coordinates[0]}
+	}
+
+	return coordinates
+}
+
+type geoJson struct {
+	Features []struct {
+		Geometry struct {
+			Coordinates []float64 `json:"coordinates"`
+		} `json:"geometry"`
+	} `json:"features"`
+}
